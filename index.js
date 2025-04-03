ac6fcbfd7380cf30f0485adfda601b94e2e7b71b
lucumt <lucumt@gmail.com>
2025-04-03 14:57:47 +0800

    update code to support low node version

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ module.exports = {
     },
     hooks: {
         'page:before': function(page) {
-            const enableDownload = this.book.config.values.pluginsConfig['mermaid-fox']['download'] ?? true;
-            console.log(enableDownload);
+            let enableDownload = true;
+            let config = this.book.config.values.pluginsConfig['mermaid-fox'];
+            if (!!config && 'download' in config) {
+                enableDownload = config['download'];
+            }
             const mermaidRegex = /^[ \t]*```\s*mermaid[ \t]*$([^`]*(?:`[^`]+)*)```$/igm;
             let download = '<svg width="15" height="15" viewBox="0 0 8 8" fill="#707070" xmlns="http://www.w3.org/2000/svg"><path d="m3 0v3h-2l3 3 3-3h-2v-3zm-3 7v1h8v-1z"/></svg>';
             let newContent = '<div><div class="mermaid">$1</div></div>';
